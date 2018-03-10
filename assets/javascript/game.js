c9e9957e929bbcb8a4a22cbe1a12f0e312472db8
Anthony Mitchell <j.ant.mitch@gmail.com>
2018-03-10 10:11:35 -0500

    final update working JS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,54 +9,102 @@ var butVal3 = (Math.floor(Math.random() * 12) + 1);
 var butVal4 = (Math.floor(Math.random() * 12) + 1);
 var ranNum = Math.floor(Math.random() * 102) +19;
 
-$(document).ready(function beginGame() {
+$(document).ready(function() {
     
     $("#ranNum").html(ranNum);
 
     $("#btn1").on("click", function(){
 
         var button1 = $(this).attr("value", butVal1);
-        console.log(butVal1);
+        playerNum += butVal1
+        $("#userScore").html(`<p> ${playerNum}</p>`);
+        
+
+        if (ranNum === playerNum){
+           
+            alert("You win!!");
+            win++;
+            $("#win").text("Wins: " + win)
+            restart();
+            }
+        else if (ranNum < playerNum){
+           
+            alert("You lose!");
+            loss++;
+            $("#loss").text(`Losses: ${loss}`);
+            restart();
+        };
+        
 });
 
     $("#btn2").on("click", function(){
 
         var button2 = $(this).attr("value", butVal2);
-        console.log(butVal3);
+        playerNum += butVal2
+        $("#userScore").html(`<p> ${playerNum}</p>`);
+        
+
+        if (ranNum === playerNum){
+           
+            alert("You win!!");
+            win++;
+            $("#win").text("Wins: " + win)
+            restart();
+            }
+        else if (ranNum < playerNum){
+           
+            alert("You lose!");
+            loss++;
+            $("#loss").text(`Losses: ${loss}`);
+            restart();
+        };
 }); 
 
     $("#btn3").on("click", function(){
 
         var button3 = $(this).attr("value", butVal3);
-        console.log(butVal3);
+        playerNum += butVal3
+        $("#userScore").html(`<p> ${playerNum}</p>`);
+        
+
+        if (ranNum === playerNum){
+           
+            alert("You win!!");
+            win++;
+            $("#win").text("Wins: " + win)
+            restart();
+            }
+        else if (ranNum < playerNum){
+           
+            alert("You lose!");
+            loss++;
+            $("#loss").text(`Losses: ${loss}`);
+            restart();
+        };
 });
 
     $("#btn4").on("click", function(){
 
         var button4 = $(this).attr("value", butVal4);
-        console.log(butVal4);
+        playerNum += butVal4
+        $("#userScore").html(`<p> ${playerNum}</p>`);
+        
+
+        if (ranNum === playerNum){
+           
+            alert("You win!!");
+            win++;
+            $("#win").text("Wins: " + win)
+            restart();
+            }
+        else if (ranNum < playerNum){
+           
+            alert("You lose!");
+            loss++;
+            $("#loss").text(`Losses: ${loss}`);
+            restart();
+        };
     });
-
-
-// while (ranNum >= playerNum){
-
-//     playerNum =  butVal1 + playerNum;
-//     $("#userScore").html(playerNum);
-// }
-    if (ranNum === playerNum){
-        alert("You win!!");
-        win++;
-        $("#win").html(loss)
-        restart();
-        }
-    else if (ranNum <= playerNum){
-        alert("You lose!");
-        loss++;
-        $("#loss").html(loss);
-        restart();
-    };
-    
- 
 
 
 function restart(){
@@ -67,8 +115,9 @@ function restart(){
     var ranNum = Math.floor(Math.random() * 102) +19;
     $("#ranNum").html(ranNum);
     playerNum = 0;
-    $("#userScore").html(playerNum);
-    beginGame();
+    $("#userScore").html(`<p>${playerNum}</p>`);
+    $("#win").text("Wins: " + win);
+    $("#loss").text(`Losses: ${loss}`);
 };
 
 });
